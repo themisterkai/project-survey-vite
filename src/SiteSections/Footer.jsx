444586adc17a390e0a24e5ca99c364ea76f919d9
Kris Albert Lee <krisalbert@gmail.com>
2023-10-26 16:26:15 +0200

    update footer text

--- src/SiteSections/Footer.jsx
+++ src/SiteSections/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="footer">
-      created with love by{' '}
+      made with love by{' '}
       <a
         href="https://github.com/themisterkai/"
         target="_blank"
