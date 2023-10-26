6bb1e7636eceb6be7dc7b8531b762e57fcd4214e
Kris Albert Lee <krisalbert@gmail.com>
2023-10-26 14:05:32 +0200

    scroll to top

--- src/SiteSections/Start.jsx
+++ src/SiteSections/Start.jsx
@@ -1,19 +1,14 @@
-// import { PropTypes } from 'prop-types';
-
 export const Start = () => {
   return (
     <div className="start">
       <div className="start-gif">
         <img src="../cat-heart.gif"></img>
-        <div />
-        <div>
-          Curious to know what's here? <p /> Click start!
-        </div>
+      </div>
+      <div>
+        Curious to know what{`'`}s here? <p /> Click start!
       </div>
     </div>
   );
 };
 
-Start.propTypes = {
-  // text: PropTypes.string.isRequired,
-};
+Start.propTypes = {};
