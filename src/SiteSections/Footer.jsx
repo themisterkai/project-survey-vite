bdca1bde89535734fbfc37522435960d6731bccf
Kris Albert Lee <krisalbert@gmail.com>
2023-10-26 16:23:15 +0200

    add footer

---
+++ src/SiteSections/Footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <div className="footer">
+      created with love by{' '}
+      <a
+        href="https://github.com/themisterkai/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Kai
+      </a>
+    </div>
+  );
+};
+
+Footer.propTypes = {};
