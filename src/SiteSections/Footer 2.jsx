65ca535e4886ebb258245d1b0c5889e8a42a4963
Kris Albert Lee <krisalbert@gmail.com>
2023-12-07 21:24:40 +0100

    render dash line only when we have historical data

---
+++ src/SiteSections/Footer 2.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <div className="footer">
+      created with love by{' '}
+      <a href="https://github.com/themisterkai/" target="_blank">
+        Kai
+      </a>
+    </div>
+  );
+};
+
+Footer.propTypes = {};
